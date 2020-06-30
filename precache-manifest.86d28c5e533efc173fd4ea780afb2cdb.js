c54f6d209eef2471c88ed47147404cfccae746a7
apurvathakker9 <atapurvathakker906@gmail.com>
2020-07-01 00:03:25 +0530

    Updates

---
+++ precache-manifest.86d28c5e533efc173fd4ea780afb2cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa0286fa3afc41dd633b0f79396e340",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "6ec3388840214b4999c0",
+    "url": "/frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9310c62691daec915e7",
+    "url": "/frontend/static/js/2.f8e90bf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend/static/js/2.f8e90bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec3388840214b4999c0",
+    "url": "/frontend/static/js/main.aa9370ad.chunk.js"
+  },
+  {
+    "revision": "92612b4edc69ba75497c",
+    "url": "/frontend/static/js/runtime-main.f5bd7888.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
