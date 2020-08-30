7b1ed1d9c1c04c6afefd8527f547ebd791ab60e8
Aditya <adigarg97@gmail.com>
2020-08-31 00:32:28 +0530

    resolve deploy error try 4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
