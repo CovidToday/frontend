d691e9c57c6ea55e3b68ef06238dde8724108132
Mohak Gupta <xmohakgupta@gmail.com>
2020-12-02 00:17:56 +0530

    Updated with new members

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -24,10 +24,10 @@ export default class About extends Component {
 									to provide a one-stop dashboard where outbreak indicators are calculated with reliable scientific methods, and are updated and
 									visualised daily to track each state’s progress in the epidemic.<br/><br/>
 
-									<b>Dr Mohak Gupta, MBBS, AIIMS Delhi</b><br />
-									<i>Interests: Technology and Data-driven Solutions in Healthcare</i><br /><br />
+									<b>Dr Mohak Gupta, MBBS, AIIMS Delhi</b><br/>
+									<i>Interests: Technology and Data-driven Solutions in Healthcare</i><br /><br/>
 
-									<b>Saptarshi Mohanta (Rishi), BS-MS, IISER Pune</b><br />
+									<b>Saptarshi Mohanta (Rishi), BS-MS, IISER Pune</b><br/>
 									<i>Interests: Computational Modelling of Biological Systems, Statistics and Data Science</i><br/><br/>
 
 									<b>Pratik Mandlecha, B.Tech CSE, IIIT Hyderabad. Data & Applied Scientist at Microsoft</b> <br/>
@@ -36,14 +36,14 @@ export default class About extends Component {
 									<b>Aditya Garg, B.Tech CSE, VIT Vellore. Software Developer at Barclays</b><br/>
 									<i>Interests: Content Creation, Web and Game Development, Playing Music</i><br/><br/>
 
-									<b>Technical Consulting and IT Support: <br/>
+									<b>Technical Consulting and IT Support: </b> <br/>
 											&nbsp;&nbsp;&nbsp;<b>Siddharth Jain, Integrated B.Tech-MBA, IIIT Gwalior</b><br/>
-											&nbsp;&nbsp;&nbsp;<i>Interests: Data Analysis, Machine Learning</i><br/>
+											&nbsp;&nbsp;&nbsp;<i>Interests: Data Analysis, Machine Learning</i><br/><br/>
 
 											&nbsp;&nbsp;&nbsp;<b>Apurva Thakker, B.Tech CSE, BFCET Bathinda</b> <br/>
 											&nbsp;&nbsp;&nbsp;<i>Interests: Solving Problems through Technology, Creating & Designing Music</i><br/><br/>
 
-											&nbsp;&nbsp;&nbsp;Abhinav Gupta, CA Inter, B.Com</b><br/>
+											&nbsp;&nbsp;&nbsp;<b>Abhinav Gupta, CA Inter, B.Com</b><br/>
 											&nbsp;&nbsp;&nbsp;<i>Interests: Simplifying Complex Structures with Technology to Make Robust and Cost Effective Systems</i><br/><br/>
 
 									<b>Devarsh Patel, BS-MS, IISER Pune</b><br/>
@@ -55,9 +55,10 @@ export default class About extends Component {
 
 
 				<Card.Body>
-					<Card.Title className="top-text-title" style={headingText}><div><strong>Public Health Advisory Panel</strong></div></Card.Title>
+					<Card.Title className="top-text-title" style={headingText}><div><strong>Expert Advisory Panel</strong></div></Card.Title>
 					<Card.Text className="top-text-body">
 						<div style={normalText}>
+										
 							<b>Dr Giridhara R Babu
 							<br/>Professor & Head, Life Course Epidemiology at Public Health Foundation of India (PHFI).
 							Wellcome Trust DBT India Alliance Fellow. Member of the Technical Advisory Committee to the Karnataka Government on COVID-19.</b>
@@ -65,6 +66,15 @@ export default class About extends Component {
 							Medical Sciences (AIIMS) New Delhi as a Junior Resident. He completed his Masters in public Health (MPH) and PhD from University of California
 							Los Angeles (UCLA), USA. Giridhara has over 60 papers published in national and international high impact journals. He has provided consultation
 							to State, National and WHO committees on various public health matters.<a href="https://www.indiaalliance.org/fellow/giridhara-r-babu"> Know more.</a>
+							<br/><br/>
+							
+							<b>Prof. Vinod Chandra Menon
+							<br/>Founder Member, National Disaster Management Authority (NDMA)</b>
+							<br/>A founding member of NDMA with the Government of India, he held a Union Minister of State designation in Government of India from 2005 to 2010 and 
+							has over 34 years working experience. He has worked in Government of Maharashtra, UNICEF India Country Office in charge of Emergency Preparedness and 
+							Response and has been a Member of the High Power Committee (HPC) on disaster management plans set up by GoI in 1999 and is currently a member of the 
+							Peer Committee on Technological Preparedness for National Disruptions set up by the Indian National Academy of Engineering. He is on the governing 
+							boards of several organizations. </a> 
 							<br/><br/>
 
 							<b>Dr Habib Hasan Farooqui
@@ -94,15 +104,6 @@ export default class About extends Component {
 					</Card.Text>
 				</Card.Body>
 
-									<Card.Body>
-										<Card.Title className="top-text-title" style={headingText}><strong>Active contributors at Covid Today</strong></Card.Title>
-										<Card.Text className="titillium">
-											<div style={normalAlignedText}>Join hands with us in this effort. Join as an active contributor <a href="https://forms.gle/HDCDVYApfRi319k58">here.</a><br/>
-												You can also visit the project's <a href="https://github.com/CovidToday/backend">GitHub</a> (open-sourced) and start contributing right away.
-											</div>
-										</Card.Text>
-									</Card.Body>
-
 						<Card.Body>
 							<Card.Title className="top-text-title" style={headingText}><div><strong>About iCART</strong></div></Card.Title>
 							<Card.Text className="top-text-body">
@@ -115,27 +116,31 @@ export default class About extends Component {
 									spanning across communities, hospitals and laboratories, which is under pilot-testing. You may follow us on <a href="https://twitter.com/icart_india">Twitter </a>
 									where we try to engage in meaningful discussions regarding the COVID-19 epidemic with fellow citizens, experts and journalists.<br/><br/>
 
-Dr Giridara Gopal, PhD Scholar, MD Community Medicine, AIIMS Delhi<br/>
-Dr Mohak Gupta, Intern Doctor, MBBS, AIIMS Delhi<br/>
-Aditi Rao, MBBS student, AIIMS Delhi<br/>
-Dev Balaji, M.Tech Bioengineering and Medical Nanotechnology, Former researcher at Harvard Medical School and IISc Bangalore<br/>
-Archisman Mazumder, MBBS student, AIIMS Delhi<br/>
-Mehak Arora, MBBS student, AIIMS Delhi<br/>
-Manraj Singh Sra, MBBS student, AIIMS Delhi<br/>
-Saptarshi Mohanta, BS-MS, IISER Pune<br/>
-Bhavik Bansal, MBBS student, AIIMS Delhi<br/>
-Dr Ayush Lohiya, Assistant Professor, Public Health, Super Specialty Cancer Institute & Hospital, Lucknow<br/>
-Dr Priyamadhaba Behera, Assistant Professor , Department of Community Medicine and Family Medicine, AIIMS Bhubaneswar<br/><br/>
-
-<i>All iCART collaborators participate in the team’s activities on a purely volunteer basis and in an individual capacity.
-	Their views and opinions under iCART represent their personal views and not that of their institute or employer.</i><br/><br/>
+<b>Dr Giridara G Gopal</b>, PhD Scholar, MD Community Medicine, AIIMS Delhi<br/>
+<b>Dr Mohak Gupta</b>, MBBS, AIIMS Delhi<br/>
+<b>Aditi Rao</b>, MBBS student, AIIMS Delhi<br/>
+<b>Rishika Mohanta</b>, BS-MS, IISER Pune<br/>
+<b>Manraj Singh Sra</b>, MBBS student, AIIMS Delhi<br/>
+<b>Archisman Mazumder</b>, MBBS student, AIIMS Delhi<br/>
+<b>Mehak Arora</b>, MBBS student, AIIMS Delhi<br/>
+<b>Amulya Gupta</b>, MBBS student, AIIMS Delhi<br/>
+<b>Bhavik Bansal</b>, MBBS student, AIIMS Delhi<br/>
+<b>Dr Ayush Lohiya</b>, Assistant Professor, Public Health, Super Specialty Cancer Institute & Hospital, Lucknow<br/>
+<b>Dr Priyamadhaba Behera</b>, Assistant Professor , Department of Community Medicine and Family Medicine, AIIMS Bhubaneswar<br/><br/>
+<b>Dev Balaji</b>, M.Tech Bioengineering and Medical Nanotechnology, Former researcher at Harvard Medical School and IISc Bangalore<br/>
+<b>Devarsh Patel</b>, BS-MS student, IISER Pune<br/>
+<b>Tanisha Mittal</b>, Masters student, Health Services Administration, University of Michigan School of Public Health<br/>
+<b>Varun Vasudevan</b>, Ph.D. Student, Institute for Computational and Mathematical Engineering, Stanford University<br/>
 
 <b>COVID-19 research from iCART:</b><br/>
 1. <a href="https://pubmed.ncbi.nlm.nih.gov/32528664/?from_single_result=SARS-CoV-2+epidemic+in+India%3A+epidemiological+features+and+in+silico+analysis+of+the+effect+of+interventions
 ">SARS-CoV-2 Epidemic in India: Epidemiological Features and in silico Analysis of the Effect of Interventions</a><br/>
 2. <a href="https://doi.org/10.1101/2020.05.13.20096826">Transmission dynamics of the COVID-19 epidemic in India and modelling optimal lockdown exit strategies</a><br/>
 3. <a href="https://www.cambridge.org/core/journals/epidemiology-and-infection/article/geographical-variation-in-case-fatality-rate-and-doubling-time-during-the-covid19-pandemic/749A8D9772342198D1B791625B66EA98">
-Geographical variation in case fatality rate and doubling time during the COVID-19 pandemic</a><br/>
+Geographical variation in case fatality rate and doubling time during the COVID-19 pandemic</a><br/><br/><br/>
+
+<i><a href="https://udaan.org/">Udaan</a> is our NGO partner. All iCART collaborators participate in the team’s activities in an individual capacity as volunteers. 
+	Their views and opinions under iCART do not necessarily represent the views of their institute or employer.</i><br/><br/>
 
 
 
